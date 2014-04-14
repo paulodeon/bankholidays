373a9364e818d998561cb6b83ab81fca082c33a8
Paul Odeon <mail@paulodeon.com>
2014-04-14 17:23:55 +0100

    Update to fix the fact that the gov site only provides one calendar now

--- lib/bankholidays.rb
+++ lib/bankholidays.rb
@@ -1,38 +1,27 @@
-require 'httparty'
-require 'uri'
-require 'icalendar'
-require 'date'
-
 module BankHolidays
-  
+
   # Get a list of all bank holidays
-  def self.all    
+  def self.all
     self.get_dates
   end
-  
+
   # Get the next bank holiday
   def self.next
     self.get_dates.reject { |x| x[:date] < Date.today }.first
   end
-  
-  private
-  
-    def self.get_dates
-      dates = []
-      year = Date.today.year
-      years = (year..(year + 10)).to_a
 
-      years.each do |y|
-        res = HTTParty.get "https://www.gov.uk/bank-holidays/england-and-wales-#{y}.ics"
-        break if res.code == 404
-        ics = Icalendar.parse( res )
-        cal = ics.first
+  private
+
+    def self.get_dates
+      res = HTTParty.get "https://www.gov.uk/bank-holidays/england-and-wales.ics"
+      ics = Icalendar.parse( res )
+      cal = ics.first
+
+      [].tap do |dates|
         cal.events.each do |e|
           dates << { :date => e.dtstart, :name => e.summary }
         end
       end
-      
-      dates
     end
-    
+
 end
